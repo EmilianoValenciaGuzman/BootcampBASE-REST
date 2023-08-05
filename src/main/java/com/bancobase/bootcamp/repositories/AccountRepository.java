12da517cfa7b0bf191d9606c6f04b016a1d2dc09
EmilianoValenciaGuzman <evalencia142000@gmail.com>
2023-08-04 21:29:27 -0600

    Tarea Creando mi servicio y consumiendo APIs Emiliano

--- src/main/java/com/bancobase/bootcamp/repositories/AccountRepository.java
+++ src/main/java/com/bancobase/bootcamp/repositories/AccountRepository.java
@@ -3,6 +3,7 @@ package com.bancobase.bootcamp.repositories;
 import com.bancobase.bootcamp.schemas.AccountSchema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.bancobase.bootcamp.schemas.CurrencySchema;
 
 import java.util.List;
 
